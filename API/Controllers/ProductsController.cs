00213e7d7910487e7e7c4af502c242dfc7015176
Onur Güneç <ogunec@hotmail.com>
2023-05-03 23:54:55 +0300

    Reading data from API included

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +7,12 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public string GetProducts()
         {
